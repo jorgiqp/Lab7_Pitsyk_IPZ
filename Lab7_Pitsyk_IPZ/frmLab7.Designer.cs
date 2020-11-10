6839f53f4c3666e90d11cf8975090ed12921132d
jorgiqp <51141836+jorgiqp@users.noreply.github.com>
2020-11-10 19:04:56 +0200

    ку

--- Lab7_Pitsyk_IPZ/frmLab7.Designer.cs
+++ Lab7_Pitsyk_IPZ/frmLab7.Designer.cs
@@ -30,33 +30,33 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.діїToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.довідкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.gBox_goods = new System.Windows.Forms.GroupBox();
-            this.gBox_info = new System.Windows.Forms.GroupBox();
-            this.gBox_bought = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.lbox_goods = new System.Windows.Forms.ListBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.cbox_automodel = new System.Windows.Forms.ComboBox();
-            this.cbox_partname = new System.Windows.Forms.ComboBox();
-            this.txt_fullname = new System.Windows.Forms.TextBox();
-            this.txt_price = new System.Windows.Forms.TextBox();
-            this.txt_hint = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txt_onStorage = new System.Windows.Forms.TextBox();
-            this.txt_total = new System.Windows.Forms.TextBox();
             this.btn_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.діїToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.btn_buy = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_clear = new System.Windows.Forms.ToolStripMenuItem();
+            this.довідкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.btn_using = new System.Windows.Forms.ToolStripMenuItem();
             this.проПрограмуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.btn_about = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.btn_clear = new System.Windows.Forms.ToolStripMenuItem();
+            this.gBox_goods = new System.Windows.Forms.GroupBox();
+            this.cbox_partname = new System.Windows.Forms.ComboBox();
+            this.cbox_automodel = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gBox_info = new System.Windows.Forms.GroupBox();
+            this.txt_onStorage = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_hint = new System.Windows.Forms.TextBox();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.txt_fullname = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gBox_bought = new System.Windows.Forms.GroupBox();
+            this.lbox_goods = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_total = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.gBox_goods.SuspendLayout();
             this.gBox_info.SuspendLayout();
@@ -86,6 +86,13 @@
             this.файлToolStripMenuItem.Size = new System.Drawing.Size(52, 21);
             this.файлToolStripMenuItem.Text = "&Файл";
             // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(180, 22);
+            this.btn_Exit.Text = "&Вихід";
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
             // діїToolStripMenuItem
             // 
             this.діїToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -96,6 +103,25 @@
             this.діїToolStripMenuItem.Size = new System.Drawing.Size(35, 21);
             this.діїToolStripMenuItem.Text = "&Дії";
             // 
+            // btn_buy
+            // 
+            this.btn_buy.Name = "btn_buy";
+            this.btn_buy.Size = new System.Drawing.Size(181, 22);
+            this.btn_buy.Text = "&Купити";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Enabled = false;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(181, 22);
+            this.toolStripMenuItem2.Text = "---------------------";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(181, 22);
+            this.btn_clear.Text = "&Очистити форму";
+            // 
             // довідкаToolStripMenuItem
             // 
             this.довідкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -105,6 +131,27 @@
             this.довідкаToolStripMenuItem.Name = "довідкаToolStripMenuItem";
             this.довідкаToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
             this.довідкаToolStripMenuItem.Text = "Дов&ідка";
+            // 
+            // btn_using
+            // 
+            this.btn_using.Name = "btn_using";
+            this.btn_using.Size = new System.Drawing.Size(186, 22);
+            this.btn_using.Text = "&Як користуватись";
+            this.btn_using.Click += new System.EventHandler(this.btn_using_Click);
+            // 
+            // проПрограмуToolStripMenuItem
+            // 
+            this.проПрограмуToolStripMenuItem.Enabled = false;
+            this.проПрограмуToolStripMenuItem.Name = "проПрограмуToolStripMenuItem";
+            this.проПрограмуToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.проПрограмуToolStripMenuItem.Text = "----------------------";
+            // 
+            // btn_about
+            // 
+            this.btn_about.Name = "btn_about";
+            this.btn_about.Size = new System.Drawing.Size(186, 22);
+            this.btn_about.Text = "&Про програму";
+            this.btn_about.Click += new System.EventHandler(this.btn_about_Click);
             // 
             // gBox_goods
             // 
@@ -118,6 +165,44 @@
             this.gBox_goods.TabIndex = 1;
             this.gBox_goods.TabStop = false;
             this.gBox_goods.Text = "Вибір товару";
+            // 
+            // cbox_partname
+            // 
+            this.cbox_partname.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbox_partname.FormattingEnabled = true;
+            this.cbox_partname.Location = new System.Drawing.Point(422, 32);
+            this.cbox_partname.Name = "cbox_partname";
+            this.cbox_partname.Size = new System.Drawing.Size(151, 25);
+            this.cbox_partname.Sorted = true;
+            this.cbox_partname.TabIndex = 1;
+            // 
+            // cbox_automodel
+            // 
+            this.cbox_automodel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbox_automodel.FormattingEnabled = true;
+            this.cbox_automodel.Location = new System.Drawing.Point(111, 32);
+            this.cbox_automodel.Name = "cbox_automodel";
+            this.cbox_automodel.Size = new System.Drawing.Size(134, 25);
+            this.cbox_automodel.Sorted = true;
+            this.cbox_automodel.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Назва автозапчастини:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Марка Авто:";
             // 
             // gBox_info
             // 
@@ -136,42 +221,60 @@
             this.gBox_info.TabStop = false;
             this.gBox_info.Text = "Інформація";
             // 
-            // gBox_bought
+            // txt_onStorage
             // 
-            this.gBox_bought.Controls.Add(this.lbox_goods);
-            this.gBox_bought.Location = new System.Drawing.Point(12, 279);
-            this.gBox_bought.Name = "gBox_bought";
-            this.gBox_bought.Size = new System.Drawing.Size(579, 153);
-            this.gBox_bought.TabIndex = 2;
-            this.gBox_bought.TabStop = false;
-            this.gBox_bought.Text = "Куплено";
+            this.txt_onStorage.Location = new System.Drawing.Point(452, 63);
+            this.txt_onStorage.Name = "txt_onStorage";
+            this.txt_onStorage.ReadOnly = true;
+            this.txt_onStorage.Size = new System.Drawing.Size(121, 23);
+            this.txt_onStorage.TabIndex = 14;
+            this.txt_onStorage.TabStop = false;
             // 
-            // label1
+            // label7
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 35);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Марка Авто:";
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(302, 66);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Залишок на складі, шт:";
             // 
-            // label2
+            // txt_hint
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(268, 35);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(148, 17);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Назва автозапчастини:";
+            this.txt_hint.Location = new System.Drawing.Point(142, 96);
+            this.txt_hint.Multiline = true;
+            this.txt_hint.Name = "txt_hint";
+            this.txt_hint.ReadOnly = true;
+            this.txt_hint.Size = new System.Drawing.Size(431, 34);
+            this.txt_hint.TabIndex = 12;
+            this.txt_hint.TabStop = false;
             // 
-            // label3
+            // txt_price
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 33);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 17);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Повна назва:";
+            this.txt_price.Location = new System.Drawing.Point(142, 63);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.ReadOnly = true;
+            this.txt_price.Size = new System.Drawing.Size(116, 23);
+            this.txt_price.TabIndex = 11;
+            this.txt_price.TabStop = false;
+            // 
+            // txt_fullname
+            // 
+            this.txt_fullname.Location = new System.Drawing.Point(142, 30);
+            this.txt_fullname.Name = "txt_fullname";
+            this.txt_fullname.ReadOnly = true;
+            this.txt_fullname.Size = new System.Drawing.Size(431, 23);
+            this.txt_fullname.TabIndex = 10;
+            this.txt_fullname.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Примітка:";
             // 
             // label4
             // 
@@ -182,14 +285,24 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Ціна за одиницю:";
             // 
-            // label5
+            // label3
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 99);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 17);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Примітка:";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Повна назва:";
+            // 
+            // gBox_bought
+            // 
+            this.gBox_bought.Controls.Add(this.lbox_goods);
+            this.gBox_bought.Location = new System.Drawing.Point(12, 279);
+            this.gBox_bought.Name = "gBox_bought";
+            this.gBox_bought.Size = new System.Drawing.Size(579, 153);
+            this.gBox_bought.TabIndex = 2;
+            this.gBox_bought.TabStop = false;
+            this.gBox_bought.Text = "Куплено";
             // 
             // lbox_goods
             // 
@@ -209,72 +322,6 @@
             this.label6.TabIndex = 4;
             this.label6.Text = "Загальна сума:";
             // 
-            // cbox_automodel
-            // 
-            this.cbox_automodel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbox_automodel.FormattingEnabled = true;
-            this.cbox_automodel.Location = new System.Drawing.Point(111, 32);
-            this.cbox_automodel.Name = "cbox_automodel";
-            this.cbox_automodel.Size = new System.Drawing.Size(134, 25);
-            this.cbox_automodel.Sorted = true;
-            this.cbox_automodel.TabIndex = 0;
-            // 
-            // cbox_partname
-            // 
-            this.cbox_partname.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbox_partname.FormattingEnabled = true;
-            this.cbox_partname.Location = new System.Drawing.Point(422, 32);
-            this.cbox_partname.Name = "cbox_partname";
-            this.cbox_partname.Size = new System.Drawing.Size(151, 25);
-            this.cbox_partname.Sorted = true;
-            this.cbox_partname.TabIndex = 1;
-            // 
-            // txt_fullname
-            // 
-            this.txt_fullname.Location = new System.Drawing.Point(142, 30);
-            this.txt_fullname.Name = "txt_fullname";
-            this.txt_fullname.ReadOnly = true;
-            this.txt_fullname.Size = new System.Drawing.Size(431, 23);
-            this.txt_fullname.TabIndex = 10;
-            this.txt_fullname.TabStop = false;
-            // 
-            // txt_price
-            // 
-            this.txt_price.Location = new System.Drawing.Point(142, 63);
-            this.txt_price.Name = "txt_price";
-            this.txt_price.ReadOnly = true;
-            this.txt_price.Size = new System.Drawing.Size(116, 23);
-            this.txt_price.TabIndex = 11;
-            this.txt_price.TabStop = false;
-            // 
-            // txt_hint
-            // 
-            this.txt_hint.Location = new System.Drawing.Point(142, 96);
-            this.txt_hint.Multiline = true;
-            this.txt_hint.Name = "txt_hint";
-            this.txt_hint.ReadOnly = true;
-            this.txt_hint.Size = new System.Drawing.Size(431, 34);
-            this.txt_hint.TabIndex = 12;
-            this.txt_hint.TabStop = false;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(302, 66);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(147, 17);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Залишок на складі, шт:";
-            // 
-            // txt_onStorage
-            // 
-            this.txt_onStorage.Location = new System.Drawing.Point(452, 63);
-            this.txt_onStorage.Name = "txt_onStorage";
-            this.txt_onStorage.ReadOnly = true;
-            this.txt_onStorage.Size = new System.Drawing.Size(121, 23);
-            this.txt_onStorage.TabIndex = 14;
-            this.txt_onStorage.TabStop = false;
-            // 
             // txt_total
             // 
             this.txt_total.Location = new System.Drawing.Point(467, 440);
@@ -283,50 +330,6 @@
             this.txt_total.Size = new System.Drawing.Size(118, 23);
             this.txt_total.TabIndex = 15;
             this.txt_total.TabStop = false;
-            // 
-            // btn_Exit
-            // 
-            this.btn_Exit.Name = "btn_Exit";
-            this.btn_Exit.Size = new System.Drawing.Size(180, 22);
-            this.btn_Exit.Text = "&Вихід";
-            // 
-            // btn_buy
-            // 
-            this.btn_buy.Name = "btn_buy";
-            this.btn_buy.Size = new System.Drawing.Size(181, 22);
-            this.btn_buy.Text = "&Купити";
-            // 
-            // btn_using
-            // 
-            this.btn_using.Name = "btn_using";
-            this.btn_using.Size = new System.Drawing.Size(186, 22);
-            this.btn_using.Text = "&Як користуватись";
-            // 
-            // проПрограмуToolStripMenuItem
-            // 
-            this.проПрограмуToolStripMenuItem.Enabled = false;
-            this.проПрограмуToolStripMenuItem.Name = "проПрограмуToolStripMenuItem";
-            this.проПрограмуToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
-            this.проПрограмуToolStripMenuItem.Text = "----------------------";
-            // 
-            // btn_about
-            // 
-            this.btn_about.Name = "btn_about";
-            this.btn_about.Size = new System.Drawing.Size(186, 22);
-            this.btn_about.Text = "&Про програму";
-            // 
-            // toolStripMenuItem2
-            // 
-            this.toolStripMenuItem2.Enabled = false;
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(181, 22);
-            this.toolStripMenuItem2.Text = "---------------------";
-            // 
-            // btn_clear
-            // 
-            this.btn_clear.Name = "btn_clear";
-            this.btn_clear.Size = new System.Drawing.Size(181, 22);
-            this.btn_clear.Text = "&Очистити форму";
             // 
             // frmLab7
             // 
--- Lab7_Pitsyk_IPZ/frmLab7.cs
+++ Lab7_Pitsyk_IPZ/frmLab7.cs
@@ -12,6 +12,7 @@ namespace Lab7_Pitsyk_IPZ
 {
     public partial class frmLab7 : Form
     {
+        public int i;
         public frmLab7()
         {
             InitializeComponent();
@@ -20,6 +21,25 @@ namespace Lab7_Pitsyk_IPZ
         private void frmLab7_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_about_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Піцик Георгій \n" + System.DateTime.Now, "Інформація", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Дійсно закрити додаток?", "Закриття додатка", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btn_using_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("", "Довідка", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
